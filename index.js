f3fdfb9633dae7d251a9cd04448e2292da3c1fbc
Zsófia Dalnoki-Papp <dalnokipapp.zsofia@gmail.com>
2018-01-09 15:00:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested .target');
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+  var liNum = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+
+  for (var i = 0; i < liNum.length; i++) {
+liNum[i].innerHTML=parseInt(liNum[i].innerHTML)+n;
+    }
+}

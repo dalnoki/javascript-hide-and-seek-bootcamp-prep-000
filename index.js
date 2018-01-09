7b0278c02aa1919d6b992ce22bffab6ed5a0ac84
Zsófia Dalnoki-Papp <dalnokipapp.zsofia@gmail.com>
2018-01-09 14:45:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function nestedTarget() {
 function deepestChild() {
   return document.querySelector('div#grand-node div div div div')
 }
+
+function increaseRankBy(n) {
+  const numbers = document.querySelector('.ranked-list')
+
+
+  for (let i = 0, i < numbers.length; i++) {
+    let numbersChildren = numbers[i].children
+
+    for (let j = 0, j < numberChildren; j++) {
+      numberChildren[j].innerHTML = parseInt(numberChildren[j].innerHTML) + n
+    }
+}
